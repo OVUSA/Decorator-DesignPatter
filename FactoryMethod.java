e4fc92debb20111516c4b3517008156f83bd4b3e
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-03-28 19:44:15 -0500

    HTMLButton.java

---
+++ FactoryMethod.java
@@ -0,0 +1,14 @@
+
+class HTMLButton implements Button{
+  
+  method render(a, b){
+  }
+        // Вернуть HTML-код кнопки.
+    method onClick(f) {
+      
+  }
+        // Навесить на кнопку об
+  
+  
+  
+}
