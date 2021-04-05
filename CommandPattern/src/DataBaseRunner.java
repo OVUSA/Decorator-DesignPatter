6054c406a5c72e8a31ac1a4d1eca508edaa49270
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-04-04 20:43:05 -0500

    Main.java

--- CommandPattern/src/DataBaseRunner.java
+++ CommandPattern/src/DataBaseRunner.java
@@ -1,5 +1,16 @@
 public class DataBaseRunner {
-    public static void maint(String [] args){
+    public static void main(String [] args){
+        Database database = new Database();
+        Developer developer = new Developer(
+                new InsertCommand(database),
+                new UpdateCommand(database),
+                new SelectCommand(database),
+                new DeleteCommand(database));
 
+
+        developer.insertRecord();
+        developer.updateRecord();
+        developer.selectRecord();
+        developer.deleteRecord();
     }
 }
